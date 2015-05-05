c9e708653ec8aaa282a8d520987a6023f82d73c4
owhc <owenc.au@gmail.com>
2015-05-05 09:19:07 +0800

    first commit

---
+++ target/scala-2.11/src_managed/main/controllers/routes.java
@@ -0,0 +1,23 @@
+// @SOURCE:D:/playfk/play-2.3.6/bmobjv2/conf/routes
+// @HASH:b31e3ed4075748bc0576b3b7654b92b97f9d84ad
+// @DATE:Tue May 05 01:13:20 CST 2015
+
+package controllers;
+
+public class routes {
+public static final controllers.ReverseAssets Assets = new controllers.ReverseAssets();
+public static final controllers.ReverseApplication Application = new controllers.ReverseApplication();
+
+public static class javascript {
+public static final controllers.javascript.ReverseAssets Assets = new controllers.javascript.ReverseAssets();
+public static final controllers.javascript.ReverseApplication Application = new controllers.javascript.ReverseApplication();
+}
+          
+
+public static class ref {
+public static final controllers.ref.ReverseAssets Assets = new controllers.ref.ReverseAssets();
+public static final controllers.ref.ReverseApplication Application = new controllers.ref.ReverseApplication();
+}
+          
+}
+          
